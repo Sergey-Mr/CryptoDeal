e69cdfe9316d70fdb8c825e5003b8c68a746a13d
Sergey-Mr <sergey.st265@gmail.com>
2023-12-16 03:07:55 +0000

    Idea addition

--- resources/views/trading.blade.php
+++ resources/views/trading.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Trading') }}
+            {{ __('Trading') }} Add the scrolling line of prices with up and down mark
         </h2>
     </x-slot>
 
