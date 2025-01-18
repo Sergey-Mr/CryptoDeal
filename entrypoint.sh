10c276e76c0cac003416509e31969d5d1df8710c
Serhii <sergey.st265@gmail.com>
2025-01-18 02:13:37 +0000

    updated entrypoint to give permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
+# Set permissions for storage and bootstrap/cache directories
+chown -R www-data:www-data /var/www/trader/storage /var/www/trader/bootstrap/cache
+chmod -R 775 /var/www/trader/storage /var/www/trader/bootstrap/cache
+
 # Wait for MySQL to be ready
 while ! mysqladmin ping -h"$DB_HOST" --silent; do
     echo "Waiting for MySQL to be ready..."
